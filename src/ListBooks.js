92d9f9cd7272caf09c1a4ceabac75c4284cdbe18
stevenliuyi <stevenliuyi@gmail.com>
2017-08-07 19:25:08 -0400

    deal with undefined shelf

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -10,7 +10,7 @@ class Book extends React.Component {
           <div className="book-top">
             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url(' + this.props.book.imageLinks.thumbnail + ')' }}></div>
             <div className="book-shelf-changer">
-              <select defaultValue={ this.props.book.shelf } onChange={(e) => this.props.onMove(this.props.book, e.target.value)}>
+              <select defaultValue={ ('shelf' in this.props.book) ? this.props.book.shelf : 'none' } onChange={(e) => this.props.onMove(this.props.book, e.target.value)}>
                 <option value="none" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
